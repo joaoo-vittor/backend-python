d64d79c7b6ccd94bbd2f0919e5320016c1bed3f0
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-07-11 20:46:52 -0300

    feat: Implementing insert_pet in PetRepository

--- src/infra/repo/pet_repository.py
+++ src/infra/repo/pet_repository.py
@@ -1,5 +1,7 @@
+from typing import List
+
 from src.domain.models import Pets
-from src.infra.entities import Pets as PetsModels
+from src.infra.entities import Pets as PetsModel
 from src.infra.config import DBConnectionHandler
 
 
@@ -19,7 +21,7 @@ class PetRepository:
 
         with DBConnectionHandler() as db_connection:
             try:
-                new_pet = PetsModels(name=name, specie=specie, age=age, user_id=user_id)
+                new_pet = PetsModel(name=name, specie=specie, age=age, user_id=user_id)
                 db_connection.session.add(new_pet)
                 db_connection.session.commit()
 
@@ -35,5 +37,53 @@ class PetRepository:
                 raise
             finally:
                 db_connection.session.close()
+
+        return None
+
+    @classmethod
+    def select_pet(cls, pet_id: int = None, user_id: int = None) -> List[Pets]:
+        """Select data in PetEntity entity by id and/or user_id
+        :param - pet_id: Id of the pet registry
+               - user_id: Id of the owner
+        :return - List with Pets selected
+        """
+
+        try:
+            query_data = None
+
+            if pet_id and not user_id:
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(PetsModel)
+                        .filter_by(id=pet_id)
+                        .one()
+                    )
+                    query_data = [data]
+
+            elif not pet_id and user_id:
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(PetsModel)
+                        .filter_by(user_id=user_id)
+                        .all()
+                    )
+                    query_data = data
+
+            elif pet_id and user_id:
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(PetsModel)
+                        .filter_by(id=pet_id, user_id=user_id)
+                        .one()
+                    )
+                    query_data = [data]
+
+            return query_data
+
+        except:
+            db_connection.session.rollback()
+            raise
+        finally:
+            db_connection.session.close()
 
         return None
