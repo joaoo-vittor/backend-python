34c495f0d5c2afd367a48c5ca6be918b811ba3a5
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-07-20 21:40:45 -0300

    feat: implementing register_user_controller and test it

---
+++ src/data/test/register_user_spy.py
@@ -0,0 +1,25 @@
+from typing import Dict
+from src.domain.models import Users
+from src.domain.test import mock_users
+
+
+class RegisterUserSpy:
+    """Class to define use case: Registe User"""
+
+    def __init__(self, user_repository: any) -> None:
+        self.user_repository = user_repository
+        self.register_param = {}
+
+    def register(self, name: str, password: str) -> Dict[bool, Users]:
+        """Register user"""
+
+        self.register_param["name"] = name
+        self.register_param["password"] = password
+
+        response = None
+        validate_entry = isinstance(name, str) and isinstance(password, str)
+
+        if validate_entry:
+            response = mock_users()
+
+        return {"Success": validate_entry, "Data": response}
